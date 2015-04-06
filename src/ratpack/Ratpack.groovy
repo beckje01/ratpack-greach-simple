36a1cfbfb6203466845c2539b906933f3f795753
Jeff Beck <beckje01@gmail.com>
2015-04-06 09:40:30 -0500

    Show Session Module

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,30 +1,36 @@
+import ratpack.session.Session
+import ratpack.session.SessionModule
+import ratpack.session.store.MapSessionsModule
+import ratpack.session.store.SessionStorage
+
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
 	bindings {
-
+		add SessionModule
+		add new MapSessionsModule(1000, 360)
 	}
 
 	handlers {
+
 		handler {
 			if (request.headers['Authorization'] != "Token faketoken") {
 				response.status(401)
 				//We must send some response or the request will hang.
 				response.send()
 			} else {
-				//We can choose to do nothing but allow the next handler in the chain to deal with the request.
-				println "next"
+
+				def sessionStorage = context.request.get(SessionStorage)
+				sessionStorage.put("example", "Galaxy")
+
 				next()
 			}
 		}
 
-
 		handler {
-			println "hellow"
-			render "Hello World"
+			def sessionStorage = context.request.get(SessionStorage)
+			def place = sessionStorage.get("example") ?: "World"
+			render "Hello $place"
 		}
-
-
-		//		assets "public"
 	}
 }
