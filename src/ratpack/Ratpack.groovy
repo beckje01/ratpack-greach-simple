cb03ef7f4df4784ac3d724307f688225ad3a7bcd
Jeff Beck <beckje01@gmail.com>
2015-04-11 09:12:50 +0200

    Twitter Example

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,22 +1,24 @@
+import org.pac4j.oauth.client.TwitterClient
 import ratpack.pac4j.Pac4jModule
-import ratpack.session.Session
 import ratpack.session.SessionModule
 import ratpack.session.store.MapSessionsModule
-import ratpack.session.store.SessionStorage
-import org.pac4j.http.client.*
 
 import static ratpack.groovy.Groovy.ratpack
 
+
 ratpack {
 	bindings {
+
+		String key = ""
+		String secret =""
 		add SessionModule
 		add new MapSessionsModule(1000, 360)
-		add new Pac4jModule(new BasicAuthClient(new DumbUsernamePasswordAuthenticator()), new SecureAllAuthorizer())
+		add new Pac4jModule(new TwitterClient(key, secret), new SecureAllAuthorizer())
 	}
 
 	handlers {
 		handler {
-			render "Hello World"
+			render "Hello Twitter"
 		}
 	}
 }
