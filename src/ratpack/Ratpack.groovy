ea3c9944c887290a7de4b522fca8cd2b54173cc3
Jeff Beck <beckje01@gmail.com>
2015-04-11 09:27:52 +0200

    Show working with the TwitterProfile

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -1,7 +1,10 @@
 import org.pac4j.oauth.client.TwitterClient
+import org.pac4j.oauth.profile.twitter.TwitterProfile
+import ratpack.handling.Context
 import ratpack.pac4j.Pac4jModule
 import ratpack.session.SessionModule
 import ratpack.session.store.MapSessionsModule
+import ratpack.session.store.SessionStorage
 
 import static ratpack.groovy.Groovy.ratpack
 
@@ -18,7 +21,16 @@ ratpack {
 
 	handlers {
 		handler {
-			render "Hello Twitter"
+			TwitterProfile profile = getUserProfile(context)
+			render "Hello ${profile.username}"
 		}
 	}
+}
+
+
+
+
+//Available by extending Pac4jProfileHandler
+protected TwitterProfile getUserProfile(Context context) {
+	return (TwitterProfile)((SessionStorage)context.getRequest().get(SessionStorage.class)).get("ratpack.pac4j-user-profile");
 }
